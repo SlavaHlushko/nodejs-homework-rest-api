2f0ab7f14779534c178c5102b40dc68f5b4038df
SlavaHlushko <slavahlushko90@gmail.com>
2023-03-14 21:43:00 +0200

    add avatar

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,19 +1,25 @@
 const { User } = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
-    res.status(409).json({ message: "Email already in use" });
+    res.status(409).json({ message: "Email in use" });
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     email: newUser.email,
+
     subscription: "starter",
   });
 };
