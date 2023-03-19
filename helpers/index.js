3000211a1c71c82c31082b4d93433d415c4de27a
SlavaHlushko <slavahlushko90@gmail.com>
2023-03-19 09:53:51 +0200

    HW-06

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,8 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
-
+const sendEmailBySG = require("./sendEmailBySG");
 module.exports = {
   ctrlWrapper,
   handleMongooseError,
+  sendEmailBySG,
 };
